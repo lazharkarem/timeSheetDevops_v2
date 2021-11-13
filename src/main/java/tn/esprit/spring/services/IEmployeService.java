bc63a10ed666589eba52c19d9dc37d262549e426
lazharkarem <karem.lazhar@gmail.com>
2021-11-13 16:46:52 +0100

    Service Empl

--- src/main/java/tn/esprit/spring/services/IEmployeService.java
+++ src/main/java/tn/esprit/spring/services/IEmployeService.java
@@ -1,5 +1,32 @@
 package tn.esprit.spring.services;
 
-public class IEmployeService {
+import java.util.List;
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.entities.Mission;
+import tn.esprit.spring.entities.Timesheet;
+import java.util.Date;
+
+
+public interface IEmployeService {
+	public int ajouterEmploye(Employe employe);
+	public void mettreAjourEmailByEmployeId(String email, int employeId);
+	public void affecterEmployeADepartement(int employeId, int depId);
+	public void desaffecterEmployeDuDepartement(int employeId, int depId);
+	//public int ajouterContrat(Contrat contrat);
+	//public void affecterContratAEmploye(int contratId, int employeId);
+	public String getEmployePrenomById(int employeId);
+	public void deleteEmployeById(int employeId);
+	//public void deleteContratById(int contratId);
+	public int getNombreEmployeJPQL();
+	public List<String> getAllEmployeNamesJPQL();
+	public List<Employe> getAllEmployeByEntreprise(Entreprise entreprise);
+	public void mettreAjourEmailByEmployeIdJPQL(String email, int employeId);
+	//public void deleteAllContratJPQL();
+	//public float getSalaireByEmployeIdJPQL(int employeId);
+	//public Double getSalaireMoyenByDepartementId(int departementId);
+	public List<Employe> getAllEmployes();
+	public List<Timesheet> getTimesheetsByMissionAndDate(Employe employe, Mission mission, 
+	Date dateDebut, Date dateFin);
 
 }
