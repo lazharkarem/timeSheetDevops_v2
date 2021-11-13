f81aa6206d1623933db1430ccee38850a1ce0eab
lazharkarem <karem.lazhar@gmail.com>
2021-11-13 04:31:23 +0100

    Add entities

---
+++ src/main/java/tn/esprit/spring/entities/Entreprise.java
@@ -0,0 +1,20 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Entreprise implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+
+
+
+
+}
