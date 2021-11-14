26d74c50e3a2ac57e998548ef75b743ac0d66f54
lazharkarem <karem.lazhar@gmail.com>
2021-11-14 09:30:22 +0100

    Update Class Moetez et Salsabil

--- src/main/java/tn/esprit/spring/repository/ContratRepository.java
+++ src/main/java/tn/esprit/spring/repository/ContratRepository.java
@@ -1,5 +1,9 @@
 package tn.esprit.spring.repository;
 
-public class ContratRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import tn.esprit.spring.entities.Contrat;
+
+public interface ContratRepository extends CrudRepository<Contrat, Integer>{
 
 }
